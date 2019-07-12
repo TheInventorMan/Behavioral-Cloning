8ffdc75287ee7983a5ac99eddf9dcbd1d29aae81
Mohammed Nasir <mnehaal@gmail.com>
2019-07-12 16:01:58 -0700

    bugfixes

--- model.py
+++ model.py
@@ -11,8 +11,12 @@ from keras.models import load_model
 # Constants
 LR_CORRECTION_FACTOR = 0.2
 CSV_FILE_PATH = "data/driving_log.csv"
+PROC_FILE_NAME = "data_processed/processed.txt"
 REL_TO_IMG = "data/"
+REL_TO_PROC = "data_processed/"
+REL_TO_PROC_FLIPPED = "data_processed_f/"
 BATCH_SIZE = 256
+preprocess = True
 
 def importData():
     # Import csv data into db
@@ -26,13 +30,116 @@ def importData():
         entry = line[:-3] # get rid of non-steering data
         db.append(entry)
         #print(entry)
-    return db
+    dataFile.close()
 
+    return db
 # Crop out landscape and car hood
 def process_img(img):
     cropped_img = img[60:130, :]
     resized_img = cv2.resize(cropped_img, (200,100))
     return resized_img
+
+def preprocessImgs():
+    db = importData()
+    processed_file = open(PROC_FILE_NAME, "w")
+    c_ctr = 0
+    l_ctr = 0
+    r_ctr = 0
+    for datapoint in db[:10]:
+        steering_ang = float(datapoint[3][1:])
+        try:
+            c_fname = datapoint[0].strip()
+            
+            c = cv2.imread(REL_TO_IMG + c_fname)
+            #print("ctype", type(c))
+            c = process_img(c)
+            cv2.imwrite(REL_TO_PROC + c_fname, c)
+            entry = str(REL_TO_PROC + c_fname + " " + str(steering_ang) + "\n")
+            processed_file.write(entry)
+
+            c_f = cv2.flip(c, 1)
+            cv2.imwrite(REL_TO_PROC_FLIPPED + c_fname, c_f)
+            entry = str(REL_TO_PROC_FLIPPED + c_fname + " " + str(-steering_ang) + "\n")
+            processed_file.write(entry)
+        except:
+            c_ctr += 1
+            pass
+
+        try:
+            l = cv2.imread(REL_TO_IMG + datapoint[1].strip())
+            #print("ltype", type(l))
+            l = process_img(l)
+            cv2.imwrite(REL_TO_PROC + datapoint[1], l)
+            entry = str(REL_TO_PROC + datapoint[1] + " " + str(steering_ang + LR_CORRECTION_FACTOR) + "\n")
+            processed_file.write(entry)
+
+            l_f = cv2.flip(l, 1)
+            cv2.imwrite(REL_TO_PROC_FLIPPED + datapoint[1], l_f)
+            entry = str(REL_TO_PROC_FLIPPED + datapoint[1] + " " + str(-steering_ang - LR_CORRECTION_FACTOR) + "\n")
+            processed_file.write(entry)
+        except:
+            l_ctr += 1
+
+
+        try:
+            r = cv2.imread(REL_TO_IMG + datapoint[2].strip())
+            #print("rtype", type(l))
+            r = process_img(r)
+            cv2.imwrite(REL_TO_PROC + datapoint[2], r)
+            entry = str(REL_TO_PROC + datapoint[2] + " " + str(steering_ang - LR_CORRECTION_FACTOR) + "\n")
+            processed_file.write(entry)
+
+            r_f = cv2.flip(r, 1)
+            cv2.imwrite(REL_TO_PROC_FLIPPED + datapoint[2], r_f)
+            entry = str(REL_TO_PROC_FLIPPED + datapoint[2] + " " + str(-steering_ang + LR_CORRECTION_FACTOR) + "\n")
+            processed_file.write(entry)
+        except:
+            r_ctr += 1
+
+    processed_file.close()
+
+
+    print(len(db)*3)
+    print("num center:", c_ctr)
+    print("num right:", r_ctr)
+    print("num left:", l_ctr)
+    print(l_ctr+c_ctr+r_ctr)
+
+    raise Exception("halt")
+        # try:
+        #     #print(datapoint)
+        #     center_img = cv2.cvtColor(cv2.imread(REL_TO_IMG + datapoint[0]), cv2.COLOR_BGR2RGB)
+        #     center_aug = process_img(center_img)
+        #     center_aug_f = cv2.flip(center_aug, 1)
+        #     X_train_batch.append(center_aug)
+        #     Y_train_batch.append(steering_ang)
+        #     X_train_batch.append(center_aug_f)
+        #     Y_train_batch.append(-steering_ang)
+        # except:
+        #     pass
+        #
+        # try:
+        #     left_img = cv2.cvtColor(cv2.imread(REL_TO_IMG + datapoint[1][1:]), cv2.COLOR_BGR2RGB)
+        #     left_aug = process_img(left_img)
+        #     left_aug_f = cv2.flip(left_aug, 1)
+        #     X_train_batch.append(left_aug)
+        #     Y_train_batch.append(steering_ang + LR_CORRECTION_FACTOR)
+        #     X_train_batch.append(left_aug_f)
+        #     Y_train_batch.append(-steering_ang - LR_CORRECTION_FACTOR)
+        # except:
+        #     pass
+        #
+        # try:
+        #     right_img = cv2.cvtColor(cv2.imread(REL_TO_IMG + datapoint[2][1:]), cv2.COLOR_BGR2RGB)
+        #     right_aug = process_img(right_img)
+        #     right_aug_f = cv2.flip(right_aug, 1)
+        #     X_train_batch.append(right_aug)
+        #     Y_train_batch.append(steering_ang - LR_CORRECTION_FACTOR)
+        #     X_train_batch.append(right_aug_f)
+        #     Y_train_batch.append(-steering_ang + LR_CORRECTION_FACTOR)
+        # except:
+        #     pass #print("o noes")
+
 
 def training_generator():
     global XY_train
@@ -124,6 +231,9 @@ def valid_generator():
                     pass
         yield (np.array(X_valid_batch), np.array(Y_valid_batch))
 
+if preprocess:
+    preprocessImgs()
+raise Exception("done")
 # Main sequence
 train_db = importData()
 XY_train, XY_valid = train_test_split(train_db, test_size=0.1)
